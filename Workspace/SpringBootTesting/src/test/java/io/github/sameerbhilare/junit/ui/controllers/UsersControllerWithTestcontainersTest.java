98a2d1c77dc8144181128511e612bac11beb8087
Sameer Bhilare <bhilare.sameer@gmail.com>
2026-01-17 22:33:15 +0530

    Integration testing with Testcontainers - creating mysql container and connecting to it

--- Workspace/SpringBootTesting/src/test/java/io/github/sameerbhilare/junit/ui/controllers/UsersControllerWithTestcontainersTest.java
+++ Workspace/SpringBootTesting/src/test/java/io/github/sameerbhilare/junit/ui/controllers/UsersControllerWithTestcontainersTest.java
@@ -1,9 +1,34 @@
 package io.github.sameerbhilare.junit.ui.controllers;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers // It is a JUnit Jupiter extension to activate automatic startup and stop of containers used in a test case.
 public class UsersControllerWithTestcontainersTest {
+
+    /**
+     * T is used to mark the containers that should be managed by the Testcontainers extension.
+     * For static containers, the TestContainer will start the container automatically before executing the first test
+     * and will stop the container after executing last test case.
+     */
+    @Container
+    private static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.4.0")
+            .withDatabaseName("photo_app")
+            .withUsername("sameer")
+            .withPassword("sameer");
+
+    // to override properties dynamically
+    @DynamicPropertySource
+    private static void overrideProperties(DynamicPropertyRegistry registry) {
+        // this is required because to avoid conflicts, mysql container will start at random port
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+    }
+
 }
