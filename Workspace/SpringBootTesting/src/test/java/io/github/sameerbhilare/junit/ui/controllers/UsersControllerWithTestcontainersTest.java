27266199abfdd2cb8c60712b234434239e30c359
Sameer Bhilare <bhilare.sameer@gmail.com>
2026-01-17 23:09:19 +0530

    Integration testing with Testcontainers - with orderd test methods

--- Workspace/SpringBootTesting/src/test/java/io/github/sameerbhilare/junit/ui/controllers/UsersControllerWithTestcontainersTest.java
+++ Workspace/SpringBootTesting/src/test/java/io/github/sameerbhilare/junit/ui/controllers/UsersControllerWithTestcontainersTest.java
@@ -1,20 +1,51 @@
 package io.github.sameerbhilare.junit.ui.controllers;
 
+import io.github.sameerbhilare.junit.security.SecurityConstants;
+import io.github.sameerbhilare.junit.ui.response.UserRest;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers // It is a JUnit Jupiter extension to activate automatic startup and stop of containers used in a test case.
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UsersControllerWithTestcontainersTest {
+
+    @Autowired
+    // If you are using the @SpringBootTest annotation with an embedded server,
+    // a TestRestTemplate and/or WebTestClient is automatically available and can be @Autowired into your test.
+    // we may inject RestTemplate, but it is safe to use TestRestTemplate in integration tests.
+    private TestRestTemplate testRestTemplate;
+
+    private String authorizationToken;
 
     /* WAY 1 - using @Container and @DynamicPropertySource =>
     // It is used to mark the containers that should be managed by the Testcontainers extension.
@@ -37,14 +68,140 @@ public class UsersControllerWithTestcontainersTest {
      */
 
     // WAY 2 - using @ServiceConnection => spring will take care of handling the mapping.
+    @Container // we can comment out @Container if we manually start()/stop() the container.
     @ServiceConnection
     private static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.4.0");
 
+    static {
+        // this ensures that container is started before any test case runs.
+        mySQLContainer.start();
+    }
+
     @Test
     @DisplayName("The MySQL container is created and is running")
+    @Order(1)
     void testContainerIsRunning() {
         assertTrue(mySQLContainer.isCreated(), "MySQL container has not been created");
         assertTrue(mySQLContainer.isRunning(), "MySQL container is not running");
     }
 
+
+    @Test
+    @DisplayName("User can be created")
+    @Order(2)
+    void testCreateUser_whenValidDetailsProvided_returnsUserDetails() throws JSONException {
+        // Arrange
+        JSONObject userDetailsRequestJson = new JSONObject();
+        userDetailsRequestJson.put("firstName", "Sameer");
+        userDetailsRequestJson.put("lastName", "Bhilare");
+        userDetailsRequestJson.put("email", "test@test.com");
+        userDetailsRequestJson.put("password","12345678");
+        userDetailsRequestJson.put("repeatPassword", "12345678");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        HttpEntity<String> request = new HttpEntity<>(userDetailsRequestJson.toString(), headers);
+
+        // Act
+        ResponseEntity<UserRest> createdUserDetailsEntity = testRestTemplate.postForEntity("/users",
+                request,
+                UserRest.class);
+        // DB - HikariDataSource automatically configures H2 database. See test server logs and search for h2
+        UserRest createdUserDetails = createdUserDetailsEntity.getBody();
+
+        // Assert
+        Assertions.assertEquals(HttpStatus.OK, createdUserDetailsEntity.getStatusCode());
+        Assertions.assertEquals(userDetailsRequestJson.getString("firstName"),
+                createdUserDetails.getFirstName(),
+                "Returned user's first name seems to be incorrect");
+        Assertions.assertEquals(userDetailsRequestJson.getString("lastName"),
+                createdUserDetails.getLastName(),
+                "Returned user's last name seems to be incorrect");
+        Assertions.assertEquals(userDetailsRequestJson.getString("email"),
+                createdUserDetails.getEmail(),
+                "Returned user's email seems to be incorrect");
+        Assertions.assertFalse(createdUserDetails.getUserId().trim().isEmpty(),
+                "User id should not be empty");
+    }
+
+    @Test
+    @DisplayName("GET /users requires JWT")
+    @Order(3)
+    void testGetUsers_whenMissingJWT_returns403() {
+        // Arrange
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", "application/json");
+
+        HttpEntity requestEntity = new HttpEntity(null, headers);
+
+        // Act
+        ResponseEntity<List<UserRest>> response = testRestTemplate.exchange("/users",
+                HttpMethod.GET,
+                requestEntity,
+                new ParameterizedTypeReference<List<UserRest>>() {
+                });
+
+        // Assert
+        Assertions.assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode(),
+                "HTTP Status code 403 Forbidden should have been returned");
+    }
+
+    @Test
+    @DisplayName("/login works")
+    @Order(4)
+    void testUserLogin_whenValidCredentialsProvided_returnsJWTinAuthorizationHeader() throws JSONException {
+        // Arrange
+//        String loginCredentialsJson = "{\n" +
+//                "    \"email\":\"test3@test.com\",\n" +
+//                "    \"password\":\"12345678\"\n" +
+//                "}";
+        JSONObject loginCredentials = new JSONObject();
+        loginCredentials.put("email","test@test.com");
+        loginCredentials.put("password","12345678");
+
+        HttpEntity<String> request = new HttpEntity<>(loginCredentials.toString());
+
+        // Act
+        ResponseEntity response = testRestTemplate.postForEntity("/users/login",
+                request,
+                null);
+
+        authorizationToken = response.getHeaders().
+                getValuesAsList(SecurityConstants.HEADER_STRING).get(0);
+
+        // Assert
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode(),
+                "HTTP Status code should be 200");
+        Assertions.assertNotNull(authorizationToken,
+                "Response should contain Authorization header with JWT");
+        Assertions.assertNotNull(response.getHeaders().
+                        getValuesAsList("UserID").get(0),
+                "Response should contain UserID in a response header");
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("GET /users works")
+    void testGetUsers_whenValidJWTProvided_returnsUsers() {
+        // Arrange
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.setBearerAuth(authorizationToken);
+
+        HttpEntity requestEntity = new HttpEntity(headers);
+
+        // Act
+        ResponseEntity<List<UserRest>> response = testRestTemplate.exchange("/users",
+                HttpMethod.GET,
+                requestEntity,
+                new ParameterizedTypeReference<List<UserRest>>() {
+                });
+
+        // Assert
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode(),
+                "HTTP Status code should be 200");
+        Assertions.assertEquals(1, response.getBody().size(), "There should be exactly 1 user in the list"); // bcz only one user is created
+    }
 }
