2c72ea20a9fed0ac2703dfe8a0b7588ddb6247b3
Sameer Bhilare <bhilare.sameer@gmail.com>
2026-01-17 22:51:15 +0530

    Integration testing with Testcontainers - testing if container is running

--- Workspace/SpringBootTesting/src/test/java/io/github/sameerbhilare/junit/ui/controllers/UsersControllerWithTestcontainersTest.java
+++ Workspace/SpringBootTesting/src/test/java/io/github/sameerbhilare/junit/ui/controllers/UsersControllerWithTestcontainersTest.java
@@ -1,11 +1,15 @@
 package io.github.sameerbhilare.junit.ui.controllers;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers // It is a JUnit Jupiter extension to activate automatic startup and stop of containers used in a test case.
@@ -29,6 +33,13 @@ public class UsersControllerWithTestcontainersTest {
         registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
         registry.add("spring.datasource.username", mySQLContainer::getUsername);
         registry.add("spring.datasource.password", mySQLContainer::getPassword);
+    }
+
+    @Test
+    @DisplayName("The MySQL container is created and is running")
+    void testContainerIsRunning() {
+        assertTrue(mySQLContainer.isCreated(), "MySQL container has not been created");
+        assertTrue(mySQLContainer.isRunning(), "MySQL container is not running");
     }
 
 }
